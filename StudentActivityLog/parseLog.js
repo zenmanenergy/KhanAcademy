be61ed0c2214a248c0ffbe0973f26879d87f08b4
Steve Nelson <snelson@zenmanenergy.org>
2020-04-06 21:09:46 -0400

    bug fixes and student edition
    A few bug fixes to the teacher version and I added a student version

---
+++ StudentActivityLog/parseLog.js
@@ -0,0 +1,142 @@
+var parseInterval;
+// setTimeout(function() {
+//     document.querySelector("._1vufyj7w button").click();
+//     setTimeout(function() {
+//         document.querySelectorAll("._1pf9vde4")[document.querySelectorAll("._1pf9vde4").length - 1].click();
+//         setTimeout(function() {
+//             document.querySelectorAll("._1ch6oaj5 DayPickerInput")[0].click();
+//             document.querySelectorAll("._168j0p10 input")[0].value = "October 13, 2019";
+
+//             setTimeout(function() {
+//                 document.querySelectorAll("._1ch6oaj5 DayPickerInput")[1].click();
+//                 document.querySelectorAll("._168j0p10 input")[1].value = "November 23, 2019";
+//                 setTimeout(function() {
+//                     document.querySelector("._ax7u0n7 button").click();
+//                     setTimeout(function() {
+//                         //parseInterval = setInterval(parsePage, 2000);
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+
+parseInterval = setInterval(parsePage, 3000);
+
+var result = "";
+var count = 0;
+var rawData =[];
+var data;
+function parsePage() {
+	console.log("parsePage");
+		console.log("---------------------------------");
+	tbody = document.querySelector("tbody");
+
+
+	for (var i = 0; i < tbody.rows.length; i++) {
+		data={};
+		//tbody.rows[i].style.backgroundColor = "yellow";
+		//result += document.querySelector("._19etdzks span").textContent;
+		if (tbody.rows[i].cells[0].querySelector("._1fiy7l25")) {
+			 	data.activity=tbody.rows[i].cells[0].querySelector("._1fiy7l25").innerText;
+		} else {
+			data.activity= "missing: title?";
+		}
+		result += "	" + data.activity;
+		if (tbody.rows[i].cells[0].querySelector("._vljmd1t path")) {
+			var type = tbody.rows[i].cells[0].querySelector("._vljmd1t path").getAttribute("d");
+			if (type.search("M7.5 6") === 0) {
+				data.type="Exercise";
+			} else if (type.search("M6 5.87") === 0) {
+				data.type="Video";
+			} else if (type.search("M5 3.53") === 0) {
+				data.type="Unit Test";
+			} else if (type.search("M11") === 0) {
+				data.type="Article";
+			} else if (type.search("M3") === 0) {
+				data.type="Project";
+			} else{
+				data.type="Unknown";
+			}
+
+		} else {
+			data.type="missing: type?";
+		}
+		result += "	" + data.type;
+		//console.log("a");
+		if (tbody.rows[i].cells[0].querySelector("._1s6kc37i")) {
+			data.subject=tbody.rows[i].cells[0].querySelector("._1s6kc37i").innerText;
+		} else {
+			data.subject="missing: subject";
+		}
+		result += "	" + data.subject;
+		//console.log("b");
+		if (tbody.rows[i].cells[1].querySelector("._19thyh0v")) {
+			data.dateString=tbody.rows[i].cells[1].querySelector("._19thyh0v").innerText.replace(" at", "");
+			data.date=new Date(data.dateString);
+		} else {
+			data.dateString="missing: date?";
+		}
+		result += "	" + data.dateString;
+		//console.log("c");
+		if (tbody.rows[i].cells[2].querySelector("._nnz187")) {
+			data.level = tbody.rows[i].cells[2].querySelector("._nnz187").innerText;
+		} else if (tbody.rows[i].cells[2].querySelector("._14hvi6g8")) {
+			data.level = tbody.rows[i].cells[2].querySelector("._14hvi6g8").innerText;
+		} else {
+			data.level = "missing: level";
+		}
+		result += "	" + data.level;
+
+		//console.log("d");
+		if (tbody.rows[i].cells[3].querySelector("._nnz187")) {
+			data.change= tbody.rows[i].cells[3].querySelector("._nnz187").innerText;
+		} else if (tbody.rows[i].cells[3].querySelector("._8czlxtv")) {
+			data.change= "improved";
+		} else {
+			data.change="<B>MISSING</b>";
+		}
+		result += "	" + data.change;
+
+		//console.log("e");
+		if (tbody.rows[i].cells[4].querySelector("._nnz187")) {
+			data.correct=tbody.rows[i].cells[4].querySelector("._nnz187").innerText;
+
+		} else {
+			data.correct=tbody.rows[i].cells[4].querySelector("._14hvi6g8").innerText;
+
+		}
+		result += "	" + data.correct;
+
+		if (tbody.rows[i].cells[5].querySelector("._14hvi6g8")) {
+			data.minutes=parseInt(tbody.rows[i].cells[5].querySelector("._14hvi6g8").innerText);
+			if (data.minutes===0){
+				data.minutes=1;
+			}
+		} else {
+			data.minutes=-1;
+		}
+
+		result += "	" + data.minutes;
+		result += "\n";
+		//console.log("g");
+		rawData[count]=data;
+		count++;
+
+	}
+	chrome.runtime.sendMessage({ count: count, result: result, done: 0 });
+	var nextbutton = document.querySelectorAll("._1mav3co7");
+	if (nextbutton.length == 2) {
+		nextbutton[1].click();
+	} else if (nextbutton.length == 1 && nextbutton[0].innerText == "Next") {
+		nextbutton[0].click();
+
+	} else {
+
+			chrome.runtime.sendMessage({ count: count, result: result, rawData:rawData,done: 1 });
+			clearInterval(parseInterval);
+	}
+}
+//+ + "	"tbody.rows[i].cells[5].querySelector("._14hvi6g8").innerText+"\n";
+//var result="";result+=tbody.rows[i].cells[0].querySelector("._1fiy7l25").innerText + "	"+ if(tbody.rows[i].cells[0].querySelector("._1s6kc37i")){tbody.rows[i].cells[0].querySelector("._1s6kc37i").innerText} + "~";};result;
---
+++ StudentActivityLog/popup.js
@@ -0,0 +1,111 @@
+
+
+document.getElementById("getDataButton").addEventListener("click", getData);
+//chrome.runtime.onMessage.addListener(onMessage);
+
+function getData(){
+
+	document.querySelector("#rawData").innerHTML = "<br><span style='background-color: yellow;font-size:20px;'>Processing...<br>Don't click anything until this message goes away</span>";
+	console.log("getdata");
+	//getStudentName();
+
+	getLogTable();
+
+}
+
+
+var student;
+function getStudentName(){
+	chrome.tabs.query({active: true}, function(tabs) {
+	  chrome.tabs.executeScript(null, {
+	    code: 'document.querySelector("._19etdzks span").textContent'
+	  }, function(results){
+		  student=results;
+		  document.querySelector("#studentName").innerHTML = "<p>Student: " + student + "</p>";
+		});
+	});
+}
+
+function getLogTable(){
+	chrome.tabs.query({active: true}, function(tabs) {
+		chrome.tabs.executeScript(null, {
+	  	file :'parseLog.js'
+	  }, function(results){
+
+
+
+		  /*
+
+			*/
+		});
+	});
+}
+
+function downloadFile(options) {
+    if(!options.url) {
+        var blob = new Blob([ options.content ], {type : "text/csv;charset=UTF-8"});
+        options.url = window.URL.createObjectURL(blob);
+    }
+    chrome.downloads.download({
+        url: options.url,
+        filename: options.filename
+    })
+}
+
+
+function message(result){
+	console.log("new message!");
+	console.log(result);
+}
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+
+	 	if (request.done){
+		  	console.log(request.count);
+		  	console.log(request.rawData);
+			var subjects=[];
+			for (var i=0;i < request.rawData.length; i++){
+
+				if (!subjects[request.rawData[i].subject]){
+					console.log("new")
+					subjects[request.rawData[i].subject]=parseInt(request.rawData[i].minutes);
+						console.log(subjects);
+				} else{
+					console.log("add")
+					console.log(subjects[request.rawData[i].subject]);
+					subjects[request.rawData[i].subject]=subjects[request.rawData[i].subject] + parseInt(request.rawData[i].minutes);
+
+				}
+			}
+	console.log(subjects);
+			var table="<table class='table' width=70% border=0 cellpadding=0 cellspacing=0>"
+			table += "<tr class=tablerow><td><b>Subject</b></td><td><b>Minutes</b></td></tr>";
+			for (var i in subjects){
+				table +="<tr class='tablerow'>";
+				table +="<td>" + i + "</td>";
+
+				table +="<td>" + subjects[i] + "</td>";
+				table +="</tr>";
+
+			}
+			table +="</table>";
+
+			document.querySelector("#rawData").innerHTML = table;
+			}
+  	// //document.querySelector("#rawData").innerHTML = request.result;
+  	// var filename=student + "-activityLog.csv";
+  	// console.log(filename);
+  	// if (request.done){
+  	// 	downloadFile({
+		//   filename: filename,
+		//   content: request.result
+		// });
+		//}
+  });
+// Download file with custom content
+
+
+// => {
+//        const lastErr = chrome.runtime.lastError;
+//        if (lastErr) console.log('tab: ' + tab.id + ' lastError: ' + JSON.stringify(lastErr));
+//    });
